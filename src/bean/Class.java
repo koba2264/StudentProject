8a26a70cd08ba2fa866b1b8ff8ae60f72692fc72
'koba' <'hrs2421446@stu.o-hara.ac.jp'>
2025-05-29 12:37:17 +0900

    学校とクラスのBeanクラスの追加

---
+++ src/bean/Class.java
@@ -0,0 +1,29 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Class implements Serializable {
+	private String id;
+	private List<School> schools = new ArrayList<>();
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public List<School> getSchools() {
+		return schools;
+	}
+	public void setSchools(List<School> schools) {
+		this.schools = schools;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
