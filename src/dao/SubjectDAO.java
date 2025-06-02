a22b3d7c05d359fc25be1def5bbb1cbd08cfd7a2
koba2264 <s2001k1022@gmail.com>
2025-06-02 11:53:19 +0900

    学生情報のDAOに検索追加

--- src/dao/SubjectDAO.java
+++ src/dao/SubjectDAO.java
@@ -29,4 +29,23 @@ public class SubjectDAO extends DAO {
 		return subject_list;
 
 	}
+
+//	受講科目の取得
+	public List<Subject> ClassSearch(String classId) throws Exception{
+		Subject subject = null;
+		List<Subject> subject_list = new ArrayList<>();
+		Connection con = getConnection();
+		PreparedStatement st = con.prepareStatement("SELECT SUB.ID AS SUB_ID, SUB.NAME AS SUB_NAME FROM (SELECT * FROM CLASS_SUBJECT  WHERE CLASS_ID = ?) AS CS INNER JOIN SUBJECT AS SUB ON CS.SUBJECT_ID = SUB.ID;");
+		st.setString(1, classId);
+		ResultSet rs = st.executeQuery();
+		while(rs.next()){
+			subject = new Subject();
+			subject.setId(rs.getString("id"));
+			subject.setSubjectName(rs.getString("name"));
+			subject_list.add(subject);
+		}
+
+		return subject_list;
+
+	}
 }
